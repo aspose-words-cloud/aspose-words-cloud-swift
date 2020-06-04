52086640e92954786b34fe0df52f867cbdfc562a
ivan.kishchenko <ivan.kishchenko@aspose.com>
2020-06-04 03:13:31 +0300

    Fix tests

--- Tests/AsposeWordsCloudTests/BuildReportTests.swift
+++ Tests/AsposeWordsCloudTests/BuildReportTests.swift
@@ -51,7 +51,7 @@ class BuildReportTests: BaseTestContext {
 
         let settings = ReportEngineSettings();
         settings.setDataSourceType(dataSourceType: ReportEngineSettings.DataSourceType.json);
-        settings.setReportBuildOptions(reportBuildOptions: [ReportBuildOptions.allowMissingMembers.rawValue, ReportBuildOptions.removeEmptyParagraphs.rawValue]);
+        settings.setReportBuildOptions(reportBuildOptions: [ReportBuildOptions.allowMissingMembers, ReportBuildOptions.removeEmptyParagraphs]);
 
         let request = BuildReportRequest(name: remoteName, data: try String(contentsOf: dataPath), reportEngineSettings: settings);
         _ = try super.getApi().buildReport(request: request);
