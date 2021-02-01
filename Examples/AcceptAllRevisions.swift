51b658697299d914393710cd508c9e14965c2ebc
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-02-01 13:18:07 +0300

    Codegen

---
+++ Examples/AcceptAllRevisions.swift
@@ -0,0 +1,13 @@
+import AsposeWordsCloud
+
+let config = Configuration(clientId: "####-####-####-####-####", clientSecret: "##################");
+let api = WordsApi(config);
+let fileName  = "test_doc.docx";
+
+// Upload original document to cloud storage.
+let uploadFileRequest = UploadFileRequest(fileContent: InputStream(url: currentDirectoryPath.appendingPathComponent(fileName, isDirectory: false))!, path: fileName);
+try api.uploadFile(request: uploadFileRequest);
+
+// Calls AcceptAllRevisions method for document in cloud.
+let request = AcceptAllRevisionsRequest(name: fileName);
+try api.acceptAllRevisions(request: request);
---
+++ Tests/AsposeWordsCloudTests/ExamplesTests.swift
@@ -0,0 +1,68 @@
+import AsposeWordsCloud
+
+import XCTest
+@testable import AsposeWordsCloud
+
+class ExamplesTests : XCTestCase
+{
+    static var allTests = [
+        ("testAcceptAllRevisions", testAcceptAllRevisions),
+        ("testAcceptAllRevisionsOnline", testAcceptAllRevisionsOnline)
+    ];
+
+    private var config : Configuration?;
+
+    override func setUp() {
+        if (self.config == nil) {
+            let baseFolder = URL(fileURLWithPath: #file)
+                .deletingLastPathComponent()
+                .deletingLastPathComponent()
+                .deletingLastPathComponent();
+
+            let credsUrl = baseFolder!
+                .appendingPathComponent("Settings", isDirectory: true)
+                .appendingPathComponent("servercreds.json", isDirectory: false);
+
+            do {
+                let credsData = try Data(contentsOf: credsUrl);
+                self.config = try ObjectSerializer.deserialize(type: Configuration.self, from: credsData);
+            }
+            catch {
+                XCTFail("File servercreds.json not found in Settings folder of project root.");
+            }
+
+            let filesUrl = baseFolder!
+                .appendingPathComponent("ExamplesData", isDirectory: true);
+
+            changeCurrentDirectoryPath(filesUrl);
+
+        }
+    }
+
+    func acceptAllRevisions() throws
+    {
+        let api = WordsApi(config);
+        let fileName  = "test_doc.docx";
+
+        // Upload original document to cloud storage.
+        let uploadFileRequest = UploadFileRequest(fileContent: InputStream(url: currentDirectoryPath.appendingPathComponent(fileName, isDirectory: false))!, path: fileName);
+        try api.uploadFile(request: uploadFileRequest);
+
+        // Calls AcceptAllRevisions method for document in cloud.
+        let request = AcceptAllRevisionsRequest(name: fileName);
+        try api.acceptAllRevisions(request: request);
+    }
+
+    func acceptAllRevisionsOnline() throws
+    {
+        let api = WordsApi(config);
+        let fileName  = "test_doc.docx";
+
+        // Calls AcceptAllRevisionsOnline method for document in cloud.
+        let request = AcceptAllRevisionsOnlineRequest(document: InputStream(url: currentDirectoryPath.appendingPathComponent(fileName, isDirectory: false))!);
+        let acceptAllRevisionsOnlineResult = try api.acceptAllRevisionsOnline(request: request);
+        await File('test_result.docx').writeAsBytes(
+            acceptAllRevisionsOnlineResult.document.buffer.asUint8List(acceptAllRevisionsOnlineResult.document.offsetInBytes, acceptAllRevisionsOnlineResult.document.lengthInBytes)
+        );
+    }
+}
