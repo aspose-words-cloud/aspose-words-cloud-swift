dd4cfa438ed77ea5fefe2d786e005b96842e5dc4
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-02-01 13:59:43 +0300

    Codegen

--- Tests/AsposeWordsCloudTests/ExamplesTests.swift
+++ Tests/AsposeWordsCloudTests/ExamplesTests.swift
@@ -63,8 +63,6 @@ class ExamplesTests : XCTestCase
         // Calls AcceptAllRevisionsOnline method for document in cloud.
         let request = AcceptAllRevisionsOnlineRequest(document: InputStream(url: currentDir!.appendingPathComponent(fileName, isDirectory: false))!);
         let acceptAllRevisionsOnlineResult = try api.acceptAllRevisionsOnline(request: request);
-        await File('test_result.docx').writeAsBytes(
-            acceptAllRevisionsOnlineResult.document.buffer.asUint8List(acceptAllRevisionsOnlineResult.document.offsetInBytes, acceptAllRevisionsOnlineResult.document.lengthInBytes)
-        );
+        acceptAllRevisionsOnlineResult.getDocument()?.write(to: currentDir!.appendingPathComponent("test_result.docx", isDirectory: false));
     }
 }
