de3b2db2bde815cb9f951a27213a1e1082406982
evgeny.ivanov <evgeny.ivanov>
2021-06-22 01:56:07 -0500

    SDK regenerated by CI server [ci skip]

--- Sources/AsposeWordsCloud/Model/Requests/CompareDocumentOnlineRequest.swift
+++ Sources/AsposeWordsCloud/Model/Requests/CompareDocumentOnlineRequest.swift
@@ -31,6 +31,7 @@ import Foundation
 public class CompareDocumentOnlineRequest : WordsApiRequest {
     private let document : InputStream;
     private let compareData : CompareData;
+    private let comparingDocument : InputStream?;
     private let loadEncoding : String?;
     private let password : String?;
     private let destFileName : String?;
@@ -38,6 +39,7 @@ public class CompareDocumentOnlineRequest : WordsApiRequest {
     private enum CodingKeys: String, CodingKey {
         case document;
         case compareData;
+        case comparingDocument;
         case loadEncoding;
         case password;
         case destFileName;
@@ -45,9 +47,10 @@ public class CompareDocumentOnlineRequest : WordsApiRequest {
     }
 
     // Initializes a new instance of the CompareDocumentOnlineRequest class.
-    public init(document : InputStream, compareData : CompareData, loadEncoding : String? = nil, password : String? = nil, destFileName : String? = nil) {
+    public init(document : InputStream, compareData : CompareData, comparingDocument : InputStream? = nil, loadEncoding : String? = nil, password : String? = nil, destFileName : String? = nil) {
         self.document = document;
         self.compareData = compareData;
+        self.comparingDocument = comparingDocument;
         self.loadEncoding = loadEncoding;
         self.password = password;
         self.destFileName = destFileName;
@@ -61,6 +64,11 @@ public class CompareDocumentOnlineRequest : WordsApiRequest {
     // Compare data.
     public func getCompareData() -> CompareData {
         return self.compareData;
+    }
+
+    // The comparing document.
+    public func getComparingDocument() -> InputStream? {
+        return self.comparingDocument;
     }
 
     // Encoding that will be used to load an HTML (or TXT) document if the encoding is not specified in HTML.
@@ -105,6 +113,10 @@ public class CompareDocumentOnlineRequest : WordsApiRequest {
          formParams.append(RequestFormParam(name: "document", body: try ObjectSerializer.serializeFile(value: self.getDocument()), contentType: "application/octet-stream"));
 
          formParams.append(RequestFormParam(name: "compareData", body: try ObjectSerializer.serialize(value: self.getCompareData()), contentType: "application/json"));
+
+         if (self.getComparingDocument() != nil) {
+             formParams.append(RequestFormParam(name: "comparingDocument", body: try ObjectSerializer.serializeFile(value: self.getComparingDocument()!), contentType: "application/octet-stream"));
+         }
 
 
          var result = WordsApiRequestData(url: urlBuilder.url!, method: "PUT");
