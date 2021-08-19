7f93dca0e4b85ef51538fdb583f85401e10ec297
evgeny.ivanov <evgeny.ivanov>
2021-08-19 05:36:53 -0500

    SDK regenerated by CI server [ci skip]

--- Examples/AcceptAllRevisions.swift
+++ Examples/AcceptAllRevisions.swift
@@ -1,12 +1,11 @@
 import AsposeWordsCloud
 
-let currentDir = ...
 let config = Configuration(clientId: "####-####-####-####-####", clientSecret: "##################");
 let api = WordsAPI(configuration: config);
 let fileName  = "test_doc.docx";
 
 // Upload original document to cloud storage.
-let myVar1 = InputStream(url: currentDir!.appendingPathComponent(fileName, isDirectory: false))!;
+let myVar1 = InputStream(url: URL(string: fileName))!;
 let myVar2 = fileName;
 let uploadFileRequest = UploadFileRequest(fileContent: myVar1, path: myVar2);
 _ = try api.uploadFile(request: uploadFileRequest);
--- Examples/AcceptAllRevisionsOnline.swift
+++ Examples/AcceptAllRevisionsOnline.swift
@@ -1,10 +1,9 @@
-let currentDir = ...
 let config = Configuration(clientId: "####-####-####-####-####", clientSecret: "##################");
 let api = WordsAPI(configuration: config);
 let fileName  = "test_doc.docx";
 
 // Calls AcceptAllRevisionsOnline method for document in cloud.
-let requestDocument = InputStream(url: currentDir!.appendingPathComponent(fileName, isDirectory: false))!;
+let requestDocument = InputStream(url: URL(string: fileName))!;
 let request = AcceptAllRevisionsOnlineRequest(document: requestDocument);
 let acceptAllRevisionsOnlineResult = try api.acceptAllRevisionsOnline(request: request);
 try acceptAllRevisionsOnlineResult.getDocument()?.write(to: currentDir!.appendingPathComponent("test_result.docx", isDirectory: false));
