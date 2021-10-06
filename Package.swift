146faabee9513fada5ac1d9479dfedca714589d2
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-10-06 18:28:40 +0300

    Manual codegen

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AsposeWordsCloud",
-            dependencies: []),
+            dependencies: ["CryptorRSA"]),
         .testTarget(
             name: "AsposeWordsCloudTests",
             dependencies: ["AsposeWordsCloud", "SwiftTestReporter"]),
