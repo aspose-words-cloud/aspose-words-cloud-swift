cea37c9f4caad0b9dc4cda73e07df6235dfc2953
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-10-07 17:04:25 +0300

    Manual codegen

--- Package.swift
+++ Package.swift
@@ -13,15 +13,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/allegro/swift-junit.git", from: "1.0.0"),
-        .package(url: "https://github.com/Kitura/BlueRSA", from: "1.0.201")
+        .package(url: "https://github.com/allegro/swift-junit.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AsposeWordsCloud",
-            dependencies: ["CryptorRSA"]),
+            dependencies: []),
         .testTarget(
             name: "AsposeWordsCloudTests",
             dependencies: ["AsposeWordsCloud", "SwiftTestReporter"]),
