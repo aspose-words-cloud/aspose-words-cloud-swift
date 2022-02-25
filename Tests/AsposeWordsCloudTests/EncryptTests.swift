d62b8bfb3666459232c8c6b5701bc9222145264d
Dmitry Finozhenok <dmitry.finozhenok@aspose.com>
2022-02-25 17:18:55 +0900

    Pretest changes

---
+++ Tests/AsposeWordsCloudTests/EncryptTests.swift
@@ -0,0 +1,58 @@
+/*
+ * --------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="EncryptTests.swift">
+ *   Copyright (c) 2022 Aspose.Words for Cloud
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------
+ */
+
+import XCTest
+@testable import AsposeWordsCloud
+
+// Example of how to use batch requests.
+@available(macOS 10.12, iOS 10.3, watchOS 3.3, tvOS 12.0, *)
+class EncryptTests: BaseTestContext {
+    static var allTests = [
+        ("testEncrypt", testEncrypt)
+    ];
+
+    // Test for encrypt.
+    func testEncrypt() throws {
+
+#if os(Linux)        
+        var thrownError: Error?
+
+        XCTAssertThrowsError(try super.getApi().encrypt("12345")) {
+            thrownError = $0
+        }
+
+        XCTAssertTrue(
+            thrownError is WordsApiError,
+            "Unexpected error type: \(type(of: thrownError))"
+        )
+#else          
+        let result = try super.getApi().encrypt("12345");
+
+        XCTAssertFalse(result.isEmpty);
+#endif
+    }
+}
