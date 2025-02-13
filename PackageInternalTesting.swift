6d102ed8b1b34081451bafefbe125048729990f4
jenkins <jenkins.aspose@gmail.com>
2025-02-13 16:01:27 -0600

    SDK regenerated by CI server [ci skip]

---
+++ PackageInternalTesting.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "AsposeWordsCloud",
+    products: [
+        .library(
+            name: "AsposeWordsCloud",
+            targets: ["AsposeWordsCloud"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/allegro/swift-junit.git", from: "1.0.0")
+    ],
+        .target(
+            name: "AsposeWordsCloud",
+            dependencies: []),
+        .testTarget(
+            name: "AsposeWordsCloudTests",
+            dependencies: [
+                "AsposeWordsCloud",
+                .product(name: "SwiftTestReporter", package: "swift-junit"),
+            ]),
+    ]
+)
+
