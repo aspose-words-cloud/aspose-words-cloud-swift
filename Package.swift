ea5b7eaebe5496c20eba44c41e3463822b27dd46
ivan.kishchenko <ivan.kishchenko@aspose.com>
2025-02-13 18:13:26 -0300

    Codegen test

--- Package.swift
+++ Package.swift
@@ -5,15 +5,12 @@ import PackageDescription
 let package = Package(
     name: "AsposeWordsCloud",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "AsposeWordsCloud",
             targets: ["AsposeWordsCloud"]),
     ],
     dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AsposeWordsCloud",
             dependencies: []),
--- PackageInternalTesting.swift
+++ PackageInternalTesting.swift
@@ -5,18 +5,13 @@ import PackageDescription
 let package = Package(
     name: "AsposeWordsCloud",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "AsposeWordsCloud",
             targets: ["AsposeWordsCloud"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/allegro/swift-junit.git", from: "1.0.0")
     ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AsposeWordsCloud",
             dependencies: []),
@@ -28,3 +23,4 @@ let package = Package(
             ]),
     ]
 )
+
