bf939201399281d6527ef1e8eaeafcdbe9dd9b07
ivan.kishchenko <ivan.kishchenko@aspose.com>
2025-02-13 17:06:01 -0300

    Refactoring Package.swift

--- PackageInternalTesting.swift
+++ PackageInternalTesting.swift
@@ -3,31 +3,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "AsposeWordsCloudHealthProdCheck",
+    name: "AsposeWordsCloud",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "AsposeWordsCloudHealthProdCheck",
-            targets: ["AsposeWordsCloudHealthProdCheck"]),
+            name: "AsposeWordsCloud",
+            targets: ["AsposeWordsCloud"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/allegro/swift-junit.git", from: "1.0.0"),
-        .package(url: "https://github.com/aspose-words-cloud/aspose-words-cloud-swift.git", .branch("release"))
+        .package(url: "https://github.com/allegro/swift-junit.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "AsposeWordsCloudHealthProdCheck",
+            name: "AsposeWordsCloud",
             dependencies: []),
         .testTarget(
-            name: "AsposeWordsCloudHealthProdCheckTests",
+            name: "AsposeWordsCloudTests",
             dependencies: [
-                "AsposeWordsCloudHealthProdCheck",
+                "AsposeWordsCloud",
                 .product(name: "SwiftTestReporter", package: "swift-junit"),
-                .product(name: "AsposeWordsCloud", package: "aspose-words-cloud-swift"),
-            ],
-        ),
+            ]),
     ]
 )
