1dbb91b79683076618e5616b1a6429ba3ecf4b39
jenkins <jenkins.aspose@gmail.com>
2025-07-10 07:37:42 -0500

    SDK regenerated by CI server [ci skip]

--- Sources/AsposeWordsCloud/Api/Configuration.swift
+++ Sources/AsposeWordsCloud/Api/Configuration.swift
@@ -187,6 +187,6 @@ public class Configuration : Codable {
 
     // Returns SDK version for using in statistics headers
     public func getSdkVersion() -> String {
-        return "25.6";
+        return "25.7";
     }
 }
