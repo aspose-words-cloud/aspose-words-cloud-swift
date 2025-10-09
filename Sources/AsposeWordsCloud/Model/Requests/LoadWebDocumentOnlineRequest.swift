0ded1e4b8dc8fb41b0acd2340eb13fda0e398902
jenkins <jenkins.aspose@gmail.com>
2025-10-09 11:47:53 -0500

    SDK regenerated by CI server [ci skip]

---
+++ Sources/AsposeWordsCloud/Model/Requests/LoadWebDocumentOnlineRequest.swift
@@ -0,0 +1,86 @@
+/*
+ * --------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="LoadWebDocumentOnlineRequest.swift">
+ *   Copyright (c) 2025 Aspose.Words for Cloud
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------
+ */
+
+import Foundation
+
+// Request model for loadWebDocumentOnline operation.
+public class LoadWebDocumentOnlineRequest : WordsApiRequest {
+    private let data : LoadWebDocumentData;
+
+    private enum CodingKeys: String, CodingKey {
+        case data;
+        case invalidCodingKey;
+    }
+
+    // Initializes a new instance of the LoadWebDocumentOnlineRequest class.
+    public init(data : LoadWebDocumentData) {
+        self.data = data;
+    }
+
+    // The properties of data downloading.
+    public func getData() -> LoadWebDocumentData {
+        return self.data;
+    }
+
+    // Creates the api request data
+    public func createApiRequestData(apiInvoker : ApiInvoker, configuration : Configuration) throws -> WordsApiRequestData {
+         var rawPath = "/words/online/put/loadWebDocument";
+         rawPath = rawPath.replacingOccurrences(of: "//", with: "/");
+
+         let urlPath = (try configuration.getApiRootUrl()).appendingPathComponent(rawPath);
+         var formParams = [RequestFormParam]();
+         var requestFilesContent = [FileReference]();
+         formParams.append(RequestFormParam(name: "data", body: try ObjectSerializer.serialize(value: self.getData()), contentType: "application/json"));
+         self.getData().collectFilesContent(&requestFilesContent);
+         try self.getData().validate();
+
+         try apiInvoker.prepareFilesContent(&requestFilesContent);
+         for requestFileReference in requestFilesContent {
+             if (requestFileReference.source == "Request") {
+                 formParams.append(RequestFormParam(name: requestFileReference.reference, body: try ObjectSerializer.serializeFile(value: requestFileReference.content), contentType: "application/octet-stream"));
+             }
+         }
+
+         var result = WordsApiRequestData(url: urlPath, method: "PUT");
+         if (formParams.count > 0) {
+             result.setBody(formParams: formParams);
+         }
+         return result;
+    }
+
+    // Deserialize response of this request
+    public func deserializeResponse(data : Data, headers : [String: String]) throws -> Any? {
+        let multipart = try ObjectSerializer.parseMultipart(data: data);
+        return LoadWebDocumentOnlineResponse(
+            model: try ObjectSerializer.deserialize(
+                type: SaveResponse.self,
+                from: (try ObjectSerializer.getMultipartByName(multipart: multipart, name: "Model")).getBody()
+            ),
+            document: try ObjectSerializer.parseFilesCollection(part: try ObjectSerializer.getMultipartByName(multipart: multipart, name: "Document"))
+        );
+    }
+}
